fcee6460837186d4b7854130d21ad41805c22e01
ctrl-MizteRy <khang.ng.h.le@gmail.com>
2025-02-26 18:44:15 -0600

    adding make-it-rain plugin

--- lua/miztery/lazy/lsp-config.lua
+++ lua/miztery/lazy/lsp-config.lua
@@ -18,6 +18,8 @@ return {
 					"harper_ls",
 					"ts_ls",
 					"jdtls",
+					"html",
+					"cssls",
 				},
 			})
 		end,
@@ -36,6 +38,9 @@ return {
 			lspconfig.ts_ls.setup({ capabilities = capabilities })
 			lspconfig.jdtls.setup({ capabilities = capabilities })
 			lspconfig.clangd.setup({ capabilities = capabilities })
+			lspconfig.bashls.setup({ capabilities = capabilities })
+			lspconfig.html.setup({ capabilities = capabilities })
+			lspconfig.cssls.setup({ capabilities = capabilities })
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
--- lua/miztery/lazy/treesitter.lua
+++ lua/miztery/lazy/treesitter.lua
@@ -3,25 +3,36 @@ return {
 	build = ":TSUpdate",
 	config = function()
 		require("nvim-treesitter.configs").setup({
-		-- A list of parser names, or "all" (the listed parsers MUST always be installed)
-		ensure_installed = {
-			"go", "java", "python", "c", "lua",
-			"vim", "vimdoc", "rust", "zig"
-		},
-		indent = {
-			enable = true
-		},
-		highlight = {
-			enable = true,
-            disable = function(lang, buf)
-                local max_filesize = 100 * 1024 -- 100 KB
-                local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                if ok and stats and stats.size > max_filesize then
-                    return true
-                end
-            end,
-            additional_vim_regex_highlighting = false,
-		},
-	})
-	end
+			-- A list of parser names, or "all" (the listed parsers MUST always be installed)
+			ensure_installed = {
+				"go",
+				"java",
+				"python",
+				"c",
+				"lua",
+				"vim",
+				"vimdoc",
+				"rust",
+				"zig",
+				"bash",
+				"javascript",
+				"html",
+				"css",
+			},
+			indent = {
+				enable = true,
+			},
+			highlight = {
+				enable = true,
+				disable = function(lang, buf)
+					local max_filesize = 100 * 1024 -- 100 KB
+					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+					if ok and stats and stats.size > max_filesize then
+						return true
+					end
+				end,
+				additional_vim_regex_highlighting = false,
+			},
+		})
+	end,
 }
