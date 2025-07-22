9586d566fc2342f269ab4fbd429d7662dabd1c97
Khang <khang.ng.h.le@gmail.com>
2025-07-22 08:18:47 -0500

    Update init.lua

--- lua/miztery/lazy/init.lua
+++ lua/miztery/lazy/init.lua
@@ -3,9 +3,5 @@ return {
 		"vhyrro/luarocks.nvim",
 		priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
 		config = true,
-	},
-	{
-		"nvim-lua/plenary.nvim",
-		name = "plenary",
-	},
+	}
 }
